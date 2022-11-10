bed982529ba7492927e979858b71bf9f740a9df7
Agustín Carballo <agustincarballo0107@gmail.com>
2022-11-10 09:37:24 -0600

    dropdown y perfil

--- ClienteWeb/src/App.js
+++ ClienteWeb/src/App.js
@@ -1,5 +1,5 @@
 import logo from './components/styles/images/logo2.png';
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter,
   Routes, // instead of "Switch"
@@ -18,9 +18,9 @@ import Footer from './components/Footer/footer';
 import Login from './components/login/login';
 import { useLocation } from 'react-router-dom';
 import Registro from './components/registro/registro';
-import  './i18n';
+import './i18n';
 import { useTranslation } from 'react-i18next';
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import 'jquery/dist/jquery.min.js';
 import $ from "jquery";
 import Popper from 'popper.js';
@@ -36,56 +36,57 @@ createStore({});
 
 function Prueba() {
   let location = useLocation();
-  if(location.pathname == "/login"){
+  if (location.pathname == "/login") {
     return <Login></Login>
   }
-  if(location.pathname == "/registro"){
+  if (location.pathname == "/registro") {
     return <Registro></Registro>
   }
-  if(location.pathname == "/error404"){
+  if (location.pathname == "/error404") {
     return <Error404></Error404>
   }
-  if(location.pathname == "/error500"){
+  if (location.pathname == "/error500") {
     return <Error500></Error500>
   }
   return (
     <React.Fragment>
-    <Sidebar></Sidebar>
-    <div className='page-wrapper'>
-    <Navbar></Navbar>
-    <Routes>
-      <Route path="/calendar" element={<Calendar />} />
-      <Route path="/paciente" element={<Paciente />} />
-      <Route path="/pacienteTest" element={<PacienteTest />} />
-      <Route path="/historiaClinica" element={<HistoriaClinica />} />
-      <Route path="/modal" element={<Modal />} />
-    </Routes>
-    <Footer></Footer>
-    </div>
-    <Helmet>
-    <script  isHydrating={true} type="text/javascript" src="./assets/vendors/core/core.js" />
-    <script  isHydrating={true} type="text/javascript" src="./assets/js/template.js" />
-    <script  isHydrating={true} type="text/javascript" src="./assets/js/chat.js" />
-    <script  isHydrating={true} type="text/javascript" src="./assets/js/dashboard-light.js" />
-    <script  isHydrating={true} type="text/javascript" src="js/jquery-steps.js" />
+      <Sidebar></Sidebar>
+      <div className='page-wrapper'>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/paciente" element={<Paciente />} />
+          <Route path="/pacienteTest" element={<PacienteTest />} />
+          <Route path="/historiaClinica" element={<HistoriaClinica />} />
+          <Route path="/modal" element={<Modal />} />
+        </Routes>
+        <Footer></Footer>
+      </div>
+      <Helmet>
+        <script isHydrating={true} type="text/javascript" src="./assets/vendors/core/core.js" />
+        <script isHydrating={true} type="text/javascript" src="./assets/js/template.js" />
+        <script isHydrating={true} type="text/javascript" src="./assets/js/dashboard-light.js" />
+        <script isHydrating={true} type="text/javascript" src="./assets/js/chat.js" />
+        <script isHydrating={true} type="text/javascript" src="./assets/js/dashboard-light.js" />
+        <script isHydrating={true} type="text/javascript" src="js/jquery-steps.js" />
 
 
-    
-</Helmet>
+
+      </Helmet>
     </React.Fragment>
   );
 }
 
 
-function App () {
+function App() {
   const { t } = useTranslation();
-    return (
-      <Suspense fallback="...is loading">
-        <BrowserRouter>
+  return (
+    <Suspense fallback="...is loading">
+      <BrowserRouter>
         <Prueba></Prueba>
-        </BrowserRouter>
-        </Suspense>
-    );
+      </BrowserRouter>
+    </Suspense>
+  );
 }
 
 
--- ClienteWeb/src/components/navbar/navbar.jsx
+++ ClienteWeb/src/components/navbar/navbar.jsx
@@ -14,48 +14,48 @@ class Navbar extends React.Component {
   render() {
     return (
       <React.Fragment>
-    <nav className="navbar">
-				<a href="#" className="sidebar-toggler">
-					<i data-feather="menu"></i>
-				</a>
-				<div className="navbar-content">
-        
-
-
-<ul className="nav nav-tabs nav-fill mt-3" role="tablist">
-                          <li className="nav-item">
-                            <a className="nav-link active" id="tab1" data-bs-toggle="tab" data-bs-target="#" role="tab" aria-controls="#" aria-selected="true">
-                              <div className="d-flex flex-row flex-lg-column flex-xl-row align-items-center justify-content-center">
-                                <p className="d-none d-sm-block">Tab1</p> &nbsp;&nbsp;
-                                <button type="button" class="btn-close" aria-label="Close"></button>
-                              </div>
-                            </a>
-                          </li>
-                          <li className="nav-item">
-                            <a className="nav-link" id="tab2" data-bs-toggle="tab" data-bs-target="#" role="tab" aria-controls="#" aria-selected="false">
-                              <div className="d-flex flex-row flex-lg-column flex-xl-row align-items-center justify-content-center">
-                                <i data-feather="phone-call" className="icon-sm me-sm-2 me-lg-0 me-xl-2 mb-md-1 mb-xl-0"></i>
-                                <p className="d-none d-sm-block">Tab1</p>
-                              </div>
-                            </a>
-                          </li>
-                          <li className="nav-item">
-                            <a className="nav-link" id="tab3" data-bs-toggle="tab" data-bs-target="#" role="tab" aria-controls="#" aria-selected="false">
-                              <div className="d-flex flex-row flex-lg-column flex-xl-row align-items-center justify-content-center">
-                                <i data-feather="phone-call" className="icon-sm me-sm-2 me-lg-0 me-xl-2 mb-md-1 mb-xl-0"></i>
-                                <p className="d-none d-sm-block">Tab3</p>
-                              </div>
-                            </a>
-                          </li>
-                          
-                        </ul>
+        <nav className="navbar">
+          <a href="#" className="sidebar-toggler">
+            <i data-feather="menu"></i>
+          </a>
+          <div className="navbar-content">
 
 
 
-				</div>
-			</nav>
-    </React.Fragment>
-    
+            <ul className="nav nav-tabs nav-fill mt-3" role="tablist">
+              <li className="nav-item">
+                <a className="nav-link active" id="tab1" data-bs-toggle="tab" data-bs-target="#" role="tab" aria-controls="#" aria-selected="true">
+                  <div className="d-flex flex-row flex-lg-column flex-xl-row align-items-center justify-content-center">
+                    <p className="d-none d-sm-block">Tab1</p> &nbsp;&nbsp;
+                    <button type="button" class="btn-close" aria-label="Close"></button>
+                  </div>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="tab2" data-bs-toggle="tab" data-bs-target="#" role="tab" aria-controls="#" aria-selected="false">
+                  <div className="d-flex flex-row flex-lg-column flex-xl-row align-items-center justify-content-center">
+                    <i data-feather="phone-call" className="icon-sm me-sm-2 me-lg-0 me-xl-2 mb-md-1 mb-xl-0"></i>
+                    <p className="d-none d-sm-block">Tab1</p>
+                  </div>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" id="tab3" data-bs-toggle="tab" data-bs-target="#" role="tab" aria-controls="#" aria-selected="false">
+                  <div className="d-flex flex-row flex-lg-column flex-xl-row align-items-center justify-content-center">
+                    <i data-feather="phone-call" className="icon-sm me-sm-2 me-lg-0 me-xl-2 mb-md-1 mb-xl-0"></i>
+                    <p className="d-none d-sm-block">Tab3</p>
+                  </div>
+                </a>
+              </li>
+
+            </ul>
+
+
+
+          </div>
+        </nav>
+      </React.Fragment>
+
     );
   }
 }
